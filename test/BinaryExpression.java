4b837494e347423cb5ac8069e53c1604e18732af
shirzohar <128406176+shirzohar@users.noreply.github.com>
2024-04-13 15:17:05 +0300

    complete

--- test/BinaryExpression.java
+++ test/BinaryExpression.java
@@ -1,4 +1,13 @@
 package test;
 
-public class BinaryExpression {
+public abstract class BinaryExpression implements Expression {
+    public Expression left;
+    public final Expression right;
+
+    public BinaryExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+
+    }
+
 }
